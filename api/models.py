eea50eacf4f7955816e3b6a8a768b3b71daed3b8
PeteRey <correoernestorey@gmail.com>
2020-06-11 11:40:12 +0200

    Modelos y DB

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.gis.db import models
+
+
+class Observation(models.Model):
+    observationId: models.IntegerField(primary_key=True)
+    datetime: models.DateTimeField()
+    geom: models.PointField()
+    altGPS: models.FloatField(default=0.0)
+    temp: models.FloatField(default=0.0)
+    hum: models.FloatField(default=0.0)
+    altBar: models.FloatField(default=0.0)
+    pressure: models.FloatField(default=0.0)
+    NO2: models.FloatField(default=0.0)
+    CO: models.FloatField(default=0.0)
+    NH3: models.FloatField(default=0.0)
+    PM1_0: models.FloatField(default=0.0)
+    PM2_5: models.FloatField(default=0.0)
+    PM10_0: models.FloatField(default=0.0)
+
 
 
