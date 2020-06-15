056673f90bfda66d1a3bfe44b64cbfb1c6ec673a
PeteRey <correoernestorey@gmail.com>
2020-06-15 16:18:38 +0200

    Corregido modelo

--- api/models.py
+++ api/models.py
@@ -3,20 +3,20 @@ from django.contrib.gis.db import models
 
 
 class Observation(models.Model):
-    observationId: models.IntegerField(primary_key=True)
-    datetime: models.DateTimeField()
-    geom: models.PointField()
-    altGPS: models.FloatField(default=0.0)
-    temp: models.FloatField(default=0.0)
-    hum: models.FloatField(default=0.0)
-    altBar: models.FloatField(default=0.0)
-    pressure: models.FloatField(default=0.0)
-    NO2: models.FloatField(default=0.0)
-    CO: models.FloatField(default=0.0)
-    NH3: models.FloatField(default=0.0)
-    PM1_0: models.FloatField(default=0.0)
-    PM2_5: models.FloatField(default=0.0)
-    PM10_0: models.FloatField(default=0.0)
+    observationId = models.IntegerField(primary_key=True)
+    datetime = models.DateTimeField()
+    geom = models.PointField()
+    altGPS = models.FloatField(default=0.0)
+    temp = models.FloatField(default=0.0)
+    hum = models.FloatField(default=0.0)
+    altBar = models.FloatField(default=0.0)
+    pressure = models.FloatField(default=0.0)
+    NO2 = models.FloatField(default=0.0)
+    CO = models.FloatField(default=0.0)
+    NH3 = models.FloatField(default=0.0)
+    PM1_0 = models.FloatField(default=0.0)
+    PM2_5 = models.FloatField(default=0.0)
+    PM10_0 = models.FloatField(default=0.0)
 
 
 
