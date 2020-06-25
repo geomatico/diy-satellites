d7a1b581466410b9dc02f866da92dca7b60272a2
PeteRey <correoernestorey@gmail.com>
2020-06-25 13:39:03 +0200

    Admin Grappelli e internacionalización

--- diysatellite/urls.py
+++ diysatellite/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 import diysatellite
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
 ]
