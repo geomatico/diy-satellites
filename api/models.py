6336ffd057387e50a689490760f922daf7157cc1
PeteRey <correoernestorey@gmail.com>
2020-06-25 11:47:32 +0200

    Activado admin

--- api/models.py
+++ api/models.py
@@ -1,11 +1,10 @@
-from django.db import models
 from django.contrib.gis.db import models
 
 
 class Observation(models.Model):
     observationId = models.IntegerField(primary_key=True)
     datetime = models.DateTimeField()
-    geom = models.GeometryField()
+    geom = models.PointField()
     altGPS = models.FloatField(default=0.0)
     temp = models.FloatField(default=0.0)
     hum = models.FloatField(default=0.0)
@@ -19,4 +18,9 @@ class Observation(models.Model):
     PM10_0 = models.FloatField(default=0.0)
 
 
-
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=50)
+    dni = models.CharField(unique=True, max_length=9)
+    password = models.CharField(max_length=20)
+    mail = models.EmailField()
