ac48f9cb05ad1fead4f80e411f7b407f13c5d1c0
PeteRey <correoernestorey@gmail.com>
2020-07-09 11:33:02 +0200

    Instalación CORS

--- diysatellite/settings.py
+++ diysatellite/settings.py
@@ -32,11 +32,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_gis',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -44,6 +46,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'diysatellite.urls'
 
