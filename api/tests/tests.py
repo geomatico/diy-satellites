cfcf0088b312783e35532eb2cc4651bf25f380c6
PeteRey <correoernestorey@gmail.com>
2020-09-21 12:46:40 +0200

    Formatos fechas y tests

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -20,35 +20,40 @@ class TestApi(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(observations['features'][0]['properties']['pm2_5'], 23.0)
-        self.assertEqual(observations['features'][17]['properties']['pm1_0'], 11.0)
+        self.assertEqual(observations['features'][15]['properties']['pm1_0'], 3328.0)
+        self.assertEqual(observations['features'][16]['properties']['pm2_5'], 2560.0)
         self.assertEqual(len(observations['features']), 260)
 
-    @skip
     def test_get_observations_filter_by_date(self):
 
         client = Client()
 
-        init_date = "2020-09-11T13:54:00.000Z"
-        end_date = "2020-09-11T13:55:00.000Z"
+        init_date = "2020-09-20T13:54:00.000Z"
+        end_date = "2020-09-23T13:55:00.000Z"
         url = '/api/v1/observations/?init_date={init_date}&end_date={end_date}'.format(init_date=init_date,
                                                                                        end_date=end_date)
 
         response = client.get(url)
         observations = response.json()
 
-        self.assertEqual(observations['features'][1]['properties']['pm10_0'], 29.0)
+        self.assertEqual(observations['features'][1]['properties']['pm10_0'], 14.0)
         self.assertEqual(len(observations['features']), 4)
         self.assertEqual(response.status_code, 200)
 
-        init_date = "2020-06-16T23:59:59.000Z"
-        end_date = "2020-06-17T00:00:29.000Z"
+        init_date = "2020-10-01T23:59:59.000Z"
+        end_date = "2020-10-01T00:00:29.000Z"
         url = '/api/v1/observations/?init_date={init_date}&end_date={end_date}'.format(init_date=init_date,
                                                                                        end_date=end_date)
         response = client.get(url)
         observations = response.json()
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(observations['features']), 0)
+        self.assertEqual(len(observations['features']), 1)
+        self.assertEqual(observations['features'][0]['properties']['pm1_0'], 18.0)
+        self.assertEqual(observations['features'][0]['properties']['pm2_5'], 28.0)
+        self.assertEqual(observations['features'][0]['properties']['pm10_0'], 28.0)
+
+
 
     @skip
     def test_upload_csv(self):
--- api/use_case.py
+++ api/use_case.py
@@ -14,7 +14,7 @@ def insert_observation_into_database(observation, username):
     pm1_0 = float(observation[constants.DB_PM1_0])
     pm2_5 = float(observation[constants.DB_PM2_5])
     pm10_0 = float(observation[constants.DB_PM10_0])
-    date = datetime.strptime(observation[constants.DB_DATE], constants.FORMAT_DATE)
+    date = datetime.strptime(observation[constants.DB_DATE], constants.DATE_FORMAT)
     time = datetime.strptime(observation[constants.DB_TIME], constants.FORMAT_TIME)
     latitude = float(observation[constants.DB_LATITUDE])
     longitude = float(observation[constants.DB_LONGITUDE])
