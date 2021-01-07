8168d4d1c87714b74698ccef033b12c51e99cb3e
PeteRey <ernesto.pete.rey@gmail.com>
2021-01-07 13:13:39 +0100

    Añadido observaciones al admin

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,11 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Observation
+from django.contrib.auth.models import Group
+
+
+class ObservationAdmin(admin.OSMGeoAdmin):
+    list_display = ('id', 'pm1_0', 'pm2_5', 'pm10_0', 'date', 'time', 'geom')
+
+
+admin.site.register(Observation, ObservationAdmin)
+admin.site.unregister(Group)
