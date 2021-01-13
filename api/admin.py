a2c2cbfe388bcee36291f009f9e39d3660f0ac3f
PeteRey <ernesto.pete.rey@gmail.com>
2021-01-13 12:19:20 +0100

    Eliminada geometria del admin

--- api/admin.py
+++ api/admin.py
@@ -3,8 +3,9 @@ from .models import Observation
 from django.contrib.auth.models import Group
 
 
-class ObservationAdmin(admin.OSMGeoAdmin):
-    list_display = ('id', 'pm1_0', 'pm2_5', 'pm10_0', 'date', 'time', 'geom')
+class ObservationAdmin(admin.ModelAdmin):
+    exclude = ('geom',)
+    list_display = ('id', 'pm1_0', 'pm2_5', 'pm10_0', 'date', 'time', 'username')
 
 
 admin.site.register(Observation, ObservationAdmin)
